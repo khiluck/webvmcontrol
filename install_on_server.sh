fa3a415f14798818580221ba29d94a6a9150c524
Oleksandr Khiliuk <khiluck@gmail.com>
2024-04-05 15:24:44 +0200

    Add nginx

--- install_on_server.sh
+++ install_on_server.sh
@@ -1,7 +1,46 @@
 #!/bin/bash
 apt-get update && apt-get -y dist-upgrade
-apt-get install -y pkg-config python3-dev gcc libvirt-dev python3-venv
+apt-get install -y pkg-config python3-dev gcc libvirt-dev python3-venv nginx
 python3 -m venv .env && source ./.env/bin/activate && pip install --upgrade pip && pip install -r requirements.txt
+
+[ -d /etc/nginx/certs ] || mkdir -p /etc/nginx/certs
+
+cat >> /etc/nginx/conf.d/webvmcontrol.conf << "EOF"
+server {
+    listen 80;
+    server_name vm.your.website;
+    return 301 https://$server_name$request_uri;
+        }
+
+server {
+    listen 443 ssl;
+    server_name vm.your.website;
+
+    ssl_certificate /etc/nginx/certs/_.your.website.crt;
+    ssl_certificate_key /etc/nginx/certs/_.your.website.key;
+
+    ssl_protocols TLSv1.2 TLSv1.3;
+    ssl_prefer_server_ciphers on;
+    ssl_ciphers "ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384";
+    ssl_session_cache shared:SSL:50m;
+    ssl_session_timeout 1d;
+    ssl_session_tickets off;
+
+    # HSTS (optional)
+    #add_header Strict-Transport-Security "max-age=31536000" always;
+
+    location / {
+        proxy_pass http://127.0.0.1:8000;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+
+    # Add configuration for static files if needed
+    }
+EOF
+
 
 ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/webvmcontrol
 
@@ -20,3 +59,5 @@ echo ""
 cat ~/.ssh/webvmcontrol.pub
 echo ""
 echo "---"
+echo ""
+echo "Copy your certs to /etc/nginx/certs folder and change the settings in /etc/nginx/conf.d/webvmcontrol.conf file!"
